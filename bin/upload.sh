8046939c5e1b9086aeee4ac914029d4fe1b346df
Shandy Brown <github@ezide.com>
2026-05-06 10:52:01 -0700

    go

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,26 +1,4 @@
 #! /bin/bash
 
-rclone sync ./public_html/ r2:www-1kfa-com/
-
-#find ./public_html -type f | while read file; do
-#  key="${file#./public_html}"
-#  echo $key
-#
-#  # set mime type based on extension
-#  case "$file" in
-#    *.svg)  mime="image/svg+xml" ;;
-#    *.html) mime="text/html" ;;
-#    *.css)  mime="text/css" ;;
-#    *.js)   mime="application/javascript" ;;
-#    *.png)  mime="image/png" ;;
-#    *.jpg|*.jpeg) mime="image/jpeg" ;;
-#    *.pdf)  mime="application/pdf" ;;
-#    *.gz|*.tgz)   mime="application/gzip" ;;
-#    *.json) mime="application/json" ;;
-#    *)      mime="application/octet-stream" ;;
-#  esac
-#
-#  echo "wrangler r2 object put --remote 'www-1kfa-com/$key' --file='$file' --content-type='$mime'"
-#  wrangler r2 object put --remote "www-1kfa-com/latest/$key" --file="$file" --content-type="$mime"
-#done
-
+echo "Cloudflare takes care of this"
+echo "https://dash.cloudflare.com/0f81f063ec196e685a8168d02d9b08f3/workers-and-pages"
